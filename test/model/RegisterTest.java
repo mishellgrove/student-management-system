f24464fe5f38c86741f48e7b261f2f0382545936
mishellgrove <70043762+mishellgrove@users.noreply.github.com>
2020-12-05 23:13:57 -0500

    test classes

--- test/model/RegisterTest.java
+++ test/model/RegisterTest.java
@@ -1,14 +1,68 @@
 package model;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import customExceptions.EntityRepeatedException;
+import customExceptions.NullEntityException;
+
 class RegisterTest {
 
+	private Register register;
+	private VirtualSchool school = new VirtualSchool("SAI");
+
+	private void setUpScenaryOne() {
+		TeacherAccount ta = new TeacherAccount("123", 0);
+		Teacher teacher = new Teacher("1", "Alfred", "Giraldo", "root", 3000000, ta, school);
+		ta.setPerson(teacher);
+		try {
+			school.addCourse("1", "Biology", "Biology for 6th grade", teacher);
+			school.addCourse("2", "History", "History for 6th grade", teacher);
+			school.addCourse("3", "Physic", "Physic for 6th grade", teacher);
+			school.addCourse("4", "Math", "Math for 6th grade", teacher);
+			StudentAccount sa = new StudentAccount("2", 600000);
+			Student student = new Student("60000", "Amanda", "Castaño", "s123", sa, school);
+			register = new Register("1", student);
+		} catch (NullEntityException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void addCourseTest() {
+		setUpScenaryOne();
+		boolean assertAux = true;
+		try {
+			register.addCourse("1");
+		} catch (NullEntityException e) {
+			assertAux = false;
+		} catch (EntityRepeatedException e) {
+			assertAux = false;
+		}
+		assertAux = register.getCourses().size() == 1;
+		assertTrue("Wrong insertion in register method", assertAux);
+	}
+
+	@Test
+	void removeCourseTest() {
+		setUpScenaryOne();
+		boolean assertAux = true;
+		int courses = 0;
+		try {
+			register.addCourse("2");
+			register.addCourse("1");
+			register.addCourse("3");
+			courses = register.getCourses().size();
+			register.removeCourse("2");
+		} catch (NullEntityException e) {
+			assertAux = false;
+		} catch (EntityRepeatedException e) {
+			assertAux = false;
+		}
+		assertAux = register.getCourses().size() == courses - 1;
+		assertTrue("Wrong remove in remove method", assertAux);
 	}
 
 }
--- test/model/TeacherTest.java
+++ test/model/TeacherTest.java
@@ -1,14 +1,89 @@
 package model;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import customExceptions.EntityRepeatedException;
+import customExceptions.NullEntityException;
+
 class TeacherTest {
 
+	private VirtualSchool school = new VirtualSchool("SAI");
+	private Teacher teacher;
+
+	private void setUpScenaryOne() {
+		TeacherAccount ta = new TeacherAccount("123", 0);
+		teacher = new Teacher("1", "Alfred", "Giraldo", "root", 3000000, ta, school);
+		ta.setPerson(teacher);
+		try {
+			school.addCourse("1", "Biology", "Biology for 6th grade");
+			school.addCourse("2", "History", "History for 6th grade");
+			school.addCourse("3", "Physic", "Physic for 6th grade");
+			school.addCourse("4", "Math", "Math for 6th grade");
+		} catch (NullEntityException e) {
+			e.printStackTrace();
+		}
+	}
+	private void setUpScenaryTwo() {
+		TeacherAccount ta = new TeacherAccount("123", 0);
+		teacher = new Teacher("1", "Alfred", "Giraldo", "root", 3000000, ta, school);
+		ta.setPerson(teacher);
+		try {
+			school.addCourse("1", "Biology", "Biology for 6th grade");
+			school.addCourse("2", "History", "History for 6th grade");
+			school.addCourse("3", "Physic", "Physic for 6th grade");
+			school.addCourse("4", "Math", "Math for 6th grade");
+
+			teacher.addCourse("3");
+			teacher.addCourse("2");
+			teacher.addCourse("1");
+			teacher.addCourse("4");
+
+		} catch (NullEntityException e) {
+			e.printStackTrace();
+		} catch (EntityRepeatedException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void addCoursesTest() {
+		setUpScenaryOne();
+		boolean assertAux = true;
+		int sizeCourses = teacher.getCourses().size();
+		try {
+
+			teacher.addCourse("1");
+			teacher.addCourse("2");
+			teacher.addCourse("3");
+			teacher.addCourse("4");
+
+		} catch (NullEntityException e) {
+			assertAux = false;
+		} catch (EntityRepeatedException e) {
+			assertAux = false;
+		}
+		assertAux = sizeCourses<teacher.getCourses().size();
+		assertTrue("Could not add the course", assertAux);
+	}
+	
+	@Test
+	void removeCourseTest() {
+		setUpScenaryTwo();
+		boolean assertAux = true;
+		
+		try {
+			teacher.removeCourse("1");
+			teacher.removeCourse("2");
+			teacher.removeCourse("4");
+		} catch (NullEntityException e) {
+			assertAux = false;
+			e.printStackTrace();
+		}
+		assertAux = teacher.getCourses().size() == 1;
+		assertTrue("Wrong in the removed course method", assertAux);
 	}
 
 }
