cf409b001f933caab2845b967e5b8f065303d649
Samuel <samuelviviescas@hotmail.com>
2020-12-07 16:36:58 -0500

    sort functions on director class added

---
+++ src/model/TeacherComparator.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.Comparator;
+
+public class TeacherComparator implements Comparator<Teacher> {
+
+	@Override
+	public int compare(Teacher o1, Teacher o2) {
+		String name1 = o1.getName();
+		String name2 = o2.getName();
+		if (name1.compareTo(name2) > 0) {
+			return 1;
+		} else if (name1.compareTo(name2) < 0) {
+			return -1;
+		}
+		return 0;
+	}
+
+}
